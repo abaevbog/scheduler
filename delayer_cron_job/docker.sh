99d09f99132cd7c5c2411dbd003fd1a27a1afc2c
Bogdan Abaev <abaevbog@grinnell.edu>
2020-07-24 10:39:08 -0400

    fixing on hold

--- delayer_cron_job/docker.sh
+++ delayer_cron_job/docker.sh
@@ -2,8 +2,9 @@ read -p "Update DELAYER?" -n 1 -r
 echo   
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     $(aws ecr get-login --no-include-email)
-    docker build -t delayer `pwd` 
+    docker build -t delayer ${DIR} 
     docker tag delayer 612185335394.dkr.ecr.us-east-1.amazonaws.com/delayer_scheduler
     docker push 612185335394.dkr.ecr.us-east-1.amazonaws.com/delayer_scheduler
 fi
--- scheduler_cron_job/docker.sh
+++ scheduler_cron_job/docker.sh
@@ -2,8 +2,9 @@ read -p "Update SCHEDULER?" -n 1 -r
 echo   
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     $(aws ecr get-login --no-include-email)
-    docker build -t scheduler `pwd` 
+    docker build -t scheduler ${DIR} 
     docker tag scheduler 612185335394.dkr.ecr.us-east-1.amazonaws.com/reminders_scheduler
     docker push 612185335394.dkr.ecr.us-east-1.amazonaws.com/reminders_scheduler
 fi
